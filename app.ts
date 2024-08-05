1d90a5f48fc5180ff5f9cceb757a0397d1603ec3
zxz19982 <adirben98@gmail.com>
2024-08-05 16:33:52 +0300

    init

---
+++ app.ts
@@ -0,0 +1,56 @@
+import express, { Express } from "express";
+const app=express();
+import mongoose from "mongoose";
+import env from "dotenv";
+env.config();
+import bodyParser from "body-parser";
+import cors from "cors";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
+
+
+
+const init = () => {
+    const promise = new Promise<Express>((resolve) => {
+      const db = mongoose.connection;
+      db.on("error", (error) => console.error(error));
+      db.once("open", () => console.log("connected to database"));
+      mongoose.connect(process.env.DATABASE_URL!).then(() => {
+        const options = {
+          definition: {
+            openapi: "3.0.0",
+            info: {
+              title: "Web Dev 2022 REST API",
+              version: "1.0.0",
+              description: "REST server including authentication using JWT",
+            },
+            servers: [{ url: "https://193.106.55.166:80" }],
+          },
+          apis: ["./src/Routes/*.ts"],
+        };
+  
+        const swaggerSpec = swaggerJsdoc(options);
+  
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(bodyParser.json());
+  
+        app.use(cors());
+  
+        app.use('/public', express.static('public'));
+  
+        app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+        // app.use("/file", fileRoute);
+        // app.use("/auth", authRouter);
+        // app.use("/comment", commentRouter);
+  
+  
+        resolve(app);
+      });
+    });
+    return promise;
+  };
+  
+  export default init;
+
+
+  
---
+++ server.ts
@@ -0,0 +1,5 @@
+import init from "./app";
+
+init().then((app) => {
+      console.log("development");
+      app.listen(process.env.PORT)});
